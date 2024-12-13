81a6db1c3bed6e0919906cd3b5a479e4a4de6ea7
unknown <dickensallinone@gmail.com>
2024-12-13 21:53:10 +0800

    add new route to display image on react app

--- src/main/java/com/example/demo/controller/NonRestrictedController.java
+++ src/main/java/com/example/demo/controller/NonRestrictedController.java
@@ -3,14 +3,20 @@ package com.example.demo.controller;
 import com.example.demo.model.dto.UserInformationInOutDto;
 import com.example.demo.model.service.UserInformationService;
 import com.example.demo.obj.UserInformationObj;
+import org.apache.commons.io.FileUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
-import org.springframework.http.ResponseEntity;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
+import org.springframework.http.*;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.List;
 
 
@@ -21,11 +27,27 @@ public class NonRestrictedController {
     @Autowired
     private UserInformationService userInformationService;
 
+    //root path for image files
+    private String FILE_PATH_ROOT = "D:/Projects/images/";
+
+
     @GetMapping("/users/record")
     public ResponseEntity<List<UserInformationObj>> usersRecord(){
         UserInformationInOutDto inDto = new UserInformationInOutDto();
         UserInformationInOutDto outDto = userInformationService.getUserAndTheirCompletedGames(inDto);
         List<UserInformationObj> userInformationObjList =  outDto.getStudentObjList();
         return new ResponseEntity<>(userInformationObjList, HttpStatus.OK);
+    }
+
+
+    @GetMapping("/images/{filename}")
+    public ResponseEntity<byte[]> getImage(@PathVariable("filename") String filename) {
+        byte[] image = new byte[0];
+        try {
+            image = FileUtils.readFileToByteArray(new File(FILE_PATH_ROOT+filename));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return ResponseEntity.ok().contentType(MediaType.IMAGE_JPEG).body(image);
     }
 }
